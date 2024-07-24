66dd919048ae900dd7bde043a6bb67cc952ced1d
tanzeem131 <dev.tanzeem@gmail.com>
2024-07-25 04:54:37 +0530

    Adding CSS to buttons.

--- app/page.tsx
+++ app/page.tsx
@@ -85,19 +85,19 @@ export default function Home() {
     <div>
       <div className="grid grid-cols-12 h-screen w-screen px-72">
         <div className="col-span-3 pl-[50px] pt-8">
-          <div className="text-[25px] h-fit w-fit hover:bg-gray-800 rounded-full cursor-pointer p-3 transition-all">
+          <div className="text-[25px] h-fit w-fit hover:bg-[#181818] rounded-full cursor-pointer p-3 transition-all">
             <BsTwitterX />
           </div>
           <div className="pr-4">
             <ul>
-            {sidebarMenuItems.map((item => <li className="w-fit flex justify-start items-center p-3 hover:bg-gray-800 rounded-full gap-4 text-2xl" key={item.title}><span>{item.icon}</span><span>{item.title}</span></li>))}
+            {sidebarMenuItems.map((item => <li className="w-fit flex justify-start items-center p-3 hover:bg-[#181818] rounded-full gap-4 text-2xl" key={item.title}><span>{item.icon}</span><span>{item.title}</span></li>))}
             </ul>
             <div className="pr-5">
-              <button className="bg-[#1A8CD8] mt-6 p-3 font-semibold rounded-full w-full text-lg">Post</button>
+              <button className="bg-[#1D9BF0] mt-6 p-3 font-semibold rounded-full w-full text-lg hover:bg-[#1A8CD8]">Post</button>
             </div>
           </div>
         </div>
-        <div className="col-span-5 border-r-[1px] border-l-[1px] border-gray-600 h-screen overflow-scroll scrollbar-hide">
+        <div className="col-span-6 border-r-[1px] border-l-[1px] border-gray-600 h-screen overflow-scroll scrollbar-hide">
           <FeedCard/>
           <FeedCard/>
           <FeedCard/>
--- components/FeedCard/index.tsx
+++ components/FeedCard/index.tsx
@@ -1,41 +1,64 @@
 import React from "react";
 import Image from "next/image";
-import {BsChat,BsHeart,BsBookmark} from 'react-icons/bs'
-import { AiOutlineRetweet } from "react-icons/ai";
+import {BsChat,BsBookmark} from 'react-icons/bs'
 import { IoIosStats } from "react-icons/io";
 import { RiShare2Line } from "react-icons/ri";
+import { IconContext } from "react-icons";
+import { PiDotsThreeOutlineFill } from "react-icons/pi";
+import { FaRetweet,FaRegHeart  } from "react-icons/fa6";
 
 
 const FeedCard: React.FC = ()=>{
     return (
-    <div className="border border-r-0 border-l-0 border-t-0 border-gray-600 border-b-[1px] p-4 hover:bg-gray-800" >
+    <div className="border border-r-0 border-l-0 border-t-0 border-gray-600 border-b-[1px] pt-3 pr-4 pl-4">
         <div className="grid grid-cols-12">
             <div className="col-span-1">
                 <Image className="rounded-full" src={'https://pbs.twimg.com/profile_images/1800082340405764096/GROpyGGE_400x400.jpg'} alt="user-img" height={50} width={50}/>
             </div>
             <div className="col-span-11">
-                <h5>Tanzeem</h5>
-                <p className="text-justify">🎉Excited to share that I just completed my Google Analytics certification! 📈✨Implemented it on my portfolio website for enhanced insights and optimization. Ready to dive deeper into #DataDriven decisions!🚀#GoogleAnalytics #WebAnalytics #connect #developer #web3 #opentowork</p>
-                <div>
-                    <Image className="rounded-lg" src={'https://pbs.twimg.com/profile_images/1800082340405764096/GROpyGGE_400x400.jpg'} alt="user-img" height={500} width={600}/>
+                <div className="flex justify-between items-start">
+                <h5 className="pl-2 font-semibold">Tanzeem</h5>
+                <IconContext.Provider value={{className: "global-class-IoIosMore" }}>
+                    <div>
+                        <PiDotsThreeOutlineFill />
+                    </div>
+                </IconContext.Provider>
                 </div>
-                <div className="flex justify-between pt-4 items-center text-lg cursor-pointer">
-                    <div className="hover:text-white hover:textshadow hover:boxshadow">
-                        <BsChat/>
-                    </div>
-                    <div className="hover:text-white hover:textshadow hover:boxshadow">
-                        <AiOutlineRetweet />
-                    </div>
-                    <div className="hover:text-white hover:textshadow hover:boxshadow">
-                        <BsHeart/>
-                    </div>
-                    <div className="hover:text-white hover:textshadow hover:boxshadow">
-                        <IoIosStats />
-                    </div>
-                    <div className="flex gap-2 ">
-                        <BsBookmark className="hover:text-white hover:textshadow hover:boxshadow"/>
-                        <RiShare2Line />
-                    </div>
+                <p className="px-2 -mt-3">🎉Excited to share that I just completed my Google Analytics certification! 📈✨Implemented it on my portfolio website for enhanced insights and optimization. Ready to dive deeper into #DataDriven decisions!🚀#GoogleAnalytics #WebAnalytics #connect #developer #web3 #opentowork</p>
+                <div className="px-2 py-1">
+                    <Image className="rounded-lg" src={'https://pbs.twimg.com/media/GP4W0AHW8AAsZnO?format=jpg&name=medium'} alt="user-img" height={400} width={600}/>
+                </div>
+                <div className="flex justify-between items-center text-lg pb-1">
+                    <IconContext.Provider value={{className: "global-class-BsChat" }}>
+                        <div >
+                            <BsChat/>
+                        </div>
+                    </IconContext.Provider>
+                    <IconContext.Provider value={{className: "global-class-AiOutlineRetweet" }}>
+                        <div className="text-[22px]">
+                            <FaRetweet/>
+                        </div>
+                    </IconContext.Provider>
+                    <IconContext.Provider value={{className: "global-class-BsHeart" }}>
+                        <div className="text-[20px]">
+                            <FaRegHeart />
+                        </div>
+                    </IconContext.Provider>
+                    <IconContext.Provider value={{className: "global-class-IoIosStats" }}>
+                        <div>
+                            <IoIosStats/>
+                        </div>
+                    </IconContext.Provider>
+                    <IconContext.Provider value={{className: "global-class-twobtn" }}>
+                        <div className="flex gap-0">
+                            <div>
+                                <BsBookmark/>
+                            </div>
+                            <div>
+                                <RiShare2Line/>
+                            </div>
+                        </div>
+                    </IconContext.Provider>
                 </div>
             </div>
         </div>
