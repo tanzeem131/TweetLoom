1211f04d96e81b0dfe3819c222f6c4aed488ae6b
tanzeem131 <dev.tanzeem@gmail.com>
2024-07-25 12:38:34 +0530

    Added CSS to buttons.

--- components/FeedCard/index.tsx
+++ components/FeedCard/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Image from "next/image";
-import {BsChat,BsBookmark} from 'react-icons/bs'
 import { IoIosStats } from "react-icons/io";
-import { RiShare2Line } from "react-icons/ri";
+import { RiShare2Line,RiChat1Line } from "react-icons/ri";
 import { IconContext } from "react-icons";
 import { PiDotsThreeOutlineFill } from "react-icons/pi";
 import { FaRetweet,FaRegHeart  } from "react-icons/fa6";
+import { GoBookmark,GoHeart  } from "react-icons/go";
 
 
 const FeedCard: React.FC = ()=>{
@@ -16,32 +16,34 @@ const FeedCard: React.FC = ()=>{
                 <Image className="rounded-full" src={'https://pbs.twimg.com/profile_images/1800082340405764096/GROpyGGE_400x400.jpg'} alt="user-img" height={50} width={50}/>
             </div>
             <div className="col-span-11">
-                <div className="flex justify-between items-start">
-                <h5 className="pl-2 font-semibold">Tanzeem</h5>
-                <IconContext.Provider value={{className: "global-class-IoIosMore" }}>
-                    <div>
-                        <PiDotsThreeOutlineFill />
-                    </div>
-                </IconContext.Provider>
+                <div className="flex justify-between">
+                    <h5 className="pl-2 font-semibold">Tanzeem</h5>
+                    <IconContext.Provider value={{className: "global-class-PiDotsThreeOutlineFill" }}>
+                        <div className="flex items-center -mt-[4px]" >
+                            <PiDotsThreeOutlineFill />
+                        </div>
+                    </IconContext.Provider>
                 </div>
-                <p className="px-2 -mt-3">🎉Excited to share that I just completed my Google Analytics certification! 📈✨Implemented it on my portfolio website for enhanced insights and optimization. Ready to dive deeper into #DataDriven decisions!🚀#GoogleAnalytics #WebAnalytics #connect #developer #web3 #opentowork</p>
+                <div className="px-2 -mt-3">
+                    <p>🎉Excited to share that I just completed my Google Analytics certification! 📈✨Implemented it on my portfolio website for enhanced insights and optimization. Ready to dive deeper into #DataDriven decisions!🚀#GoogleAnalytics #WebAnalytics #connect #developer #web3 #opentowork</p>
+                </div>
                 <div className="px-2 py-1">
                     <Image className="rounded-lg" src={'https://pbs.twimg.com/media/GP4W0AHW8AAsZnO?format=jpg&name=medium'} alt="user-img" height={400} width={600}/>
                 </div>
                 <div className="flex justify-between items-center text-lg pb-1">
-                    <IconContext.Provider value={{className: "global-class-BsChat" }}>
-                        <div >
-                            <BsChat/>
+                    <IconContext.Provider value={{className: "global-class-RiChat1Line" }}>
+                        <div>
+                            <RiChat1Line/>
                         </div>
                     </IconContext.Provider>
-                    <IconContext.Provider value={{className: "global-class-AiOutlineRetweet" }}>
-                        <div className="text-[22px]">
+                    <IconContext.Provider value={{className: "global-class-FaRetweet" }}>
+                        <div>
                             <FaRetweet/>
                         </div>
                     </IconContext.Provider>
-                    <IconContext.Provider value={{className: "global-class-BsHeart" }}>
-                        <div className="text-[20px]">
-                            <FaRegHeart />
+                    <IconContext.Provider value={{className: "global-class-GoHeart" }}>
+                        <div>
+                            <GoHeart />
                         </div>
                     </IconContext.Provider>
                     <IconContext.Provider value={{className: "global-class-IoIosStats" }}>
@@ -52,7 +54,7 @@ const FeedCard: React.FC = ()=>{
                     <IconContext.Provider value={{className: "global-class-twobtn" }}>
                         <div className="flex gap-0">
                             <div>
-                                <BsBookmark/>
+                                <GoBookmark/>
                             </div>
                             <div>
                                 <RiShare2Line/>
